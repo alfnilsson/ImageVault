8a8699b78ded996f899e73dd299fbac3df39fdd3
mail@alfnilsson.se <mail@alfnilsson.se>
2017-12-07 23:03:21 +0100

    Added Imagevault workaround

---
+++ ImageVault.EPiServer.UI 5.5.36/ImageVaultScriptWorkaround/InsertMediaScriptHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Toders.ImageVaultScriptWorkaround
+{
+    using System.Web;
+
+    public class InsertMediaScriptHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Clear();
+            context.Response.ContentType = "text/plain; charset=utf-8";
+            context.Response.Flush();
+            context.Response.WriteFile(context.Server.MapPath("~/ImageVaultScriptWorkaround/insertmedia.min.js"));
+            context.Response.End();
+        }
+    }
+}
